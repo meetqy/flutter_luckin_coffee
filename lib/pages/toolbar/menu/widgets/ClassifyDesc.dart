00bb00dec5f2026cb33db250ecd718bdc748c1a0
meetqy <37372842@qq.com>
2019-08-12 10:43:11 +0800

    no message

--- lib/pages/toolbar/menu/widgets/ClassifyDesc.dart
+++ lib/pages/toolbar/menu/widgets/ClassifyDesc.dart
@@ -1,7 +1,7 @@
 /*
  * @Author: meetqy
  * @since: 2019-08-10 17:19:04
- * @lastTime: 2019-08-10 17:25:20
+ * @lastTime: 2019-08-12 10:42:54
  * @LastEditors: meetqy
  */
 import 'package:color_dart/color_dart.dart';
@@ -11,7 +11,7 @@ class ClassifyDesc extends StatelessWidget {
   final String name;
   final String desc;
 
-  /// 创建商品列表显示的分类介绍
+  /// 创建商品列表显示的分类介绍 每一类商品顶部有一个对该类商品的介绍
   /// ```
   /// @param {String} name - 名字
   /// @param {String} desc - 描述

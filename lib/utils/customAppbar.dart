3607a9f8ea0921dc0265b6f32e7f801e584077e1
meetqy <37372842@qq.com>
2019-08-20 16:57:38 +0800

    封装appbar

---
+++ lib/utils/customAppbar.dart
@@ -0,0 +1,29 @@
+import 'package:color_dart/color_dart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_luckin_coffee/utils/Icon.dart';
+
+/// 通用appbar
+/// 
+/// ```
+/// @param {String} title - 标题
+/// ```
+AppBar customAppbar(BuildContext context, {
+  String title,
+  double elevation = 0,
+}) {
+  return AppBar(
+    title: Text(title, style: TextStyle(
+      color: rgba(56, 56, 56, 1),
+      fontSize: 18,
+      fontWeight: FontWeight.bold
+    ),),
+    backgroundColor: hex('#fff'),
+    elevation: elevation,
+    leading: InkWell(
+      child: icontupian8(color: rgba(44, 44, 44, 1), size: 16),
+      onTap: (){
+        Navigator.pop(context);
+      },
+    )
+  );
+}

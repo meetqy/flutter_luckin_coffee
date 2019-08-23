5569968a3545ea88314da3e96af9fa578b49ea8e
meetqy <37372842@qq.com>
2019-08-23 18:01:18 +0800

    修改
    商品选择规格弹窗在小屏幕上面显示不完整

--- lib/widgets/DialogPage/index.dart
+++ lib/widgets/DialogPage/index.dart
@@ -61,112 +61,113 @@ final dialogPage = FullScreenDialog(
       ],),
 
       // 商品信息 滚动部分
-      Container(
-        color: hex('#fff'),
-        height: 310,
-        padding: EdgeInsets.only(top: 12),
-        child: SingleChildScrollView(
-          child: Column(children: <Widget>[
-            // 选择口味 大小等
-            Container(child: 
-              Column(children: <Widget>[
-                SelectRow(
-                  "规格",
-                  rightContainer: <Widget>[
-                    RadiusBtn('大', isActive: true,),
-                    RadiusBtn('小', isActive: false,),
-                  ],
-                ),
-                SelectRow(
-                  "温度",
-                  rightContainer: <Widget>[
-                    RadiusBtn('冰', isActive: false,),
-                    RadiusBtn('热', isActive: true,),
-                  ],
-                ),
-                SelectRow(
-                  "糖",
-                  rightContainer: <Widget>[
-                    RadiusBtn('无糖', isActive: false,),
-                    RadiusBtn('半塘', isActive: true,),
-                    RadiusBtn('单份糖', isActive: false,),
-                    RadiusBtn('香草3元', isActive: false,),
-                    RadiusBtn('榛子3元', isActive: false,),
-                  ],
-                ),
-                SelectRow(
-                  "奶",
-                  rightContainer: <Widget>[
-                    RadiusBtn('无奶', isActive: true,),
-                    RadiusBtn('单份奶', isActive: false,),
-                    RadiusBtn('双份奶', isActive: false,),
-                  ],
-                ),
-              ],),
-            ),
+      Expanded(
+        child: Container(
+          color: hex('#fff'),
+          padding: EdgeInsets.only(top: 12),
+          child: SingleChildScrollView(
+            child: Column(children: <Widget>[
+              // 选择口味 大小等
+              Container(child: 
+                Column(children: <Widget>[
+                  SelectRow(
+                    "规格",
+                    rightContainer: <Widget>[
+                      RadiusBtn('大', isActive: true,),
+                      RadiusBtn('小', isActive: false,),
+                    ],
+                  ),
+                  SelectRow(
+                    "温度",
+                    rightContainer: <Widget>[
+                      RadiusBtn('冰', isActive: false,),
+                      RadiusBtn('热', isActive: true,),
+                    ],
+                  ),
+                  SelectRow(
+                    "糖",
+                    rightContainer: <Widget>[
+                      RadiusBtn('无糖', isActive: false,),
+                      RadiusBtn('半塘', isActive: true,),
+                      RadiusBtn('单份糖', isActive: false,),
+                      RadiusBtn('香草3元', isActive: false,),
+                      RadiusBtn('榛子3元', isActive: false,),
+                    ],
+                  ),
+                  SelectRow(
+                    "奶",
+                    rightContainer: <Widget>[
+                      RadiusBtn('无奶', isActive: true,),
+                      RadiusBtn('单份奶', isActive: false,),
+                      RadiusBtn('双份奶', isActive: false,),
+                    ],
+                  ),
+                ],),
+              ),
 
-            // 分隔线
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: ui.borderBottom()
+              // 分隔线
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: ui.borderBottom()
+                  ),
                 ),
               ),
-            ),
 
-            // 大咖说
-            Container(
-              child: Column(children: <Widget>[
-                Row(children: <Widget>[
-                  Expanded(child: 
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15, right: 15),
-                      child: Text('大咖说', style: TextStyle(color: rgba(56, 56, 56, 1),fontSize: 13),),
-                    )
-                  ,),
-                ],),
-
-                Column(children: <Widget>[
-                  DacaiSay(
-                    avatar: 'lib/assets/images/menu/author1.jpg',
-                    name: 'Andrea Lattuada',
-                    label: 'WBC意大利冠军',
-                    desc: '咖啡好味道，生都八分。研磨、烘焙、虫蛀、萃取，我愿意就这样，用医生，去打磨一杯咖啡的乡村',
-                    time: '2018-09-22 10:22'
-                  ),
-                ],)
-              ],),  
-            ),
-          
-            // 商品描述
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
+              // 大咖说
+              Container(
+                child: Column(children: <Widget>[
                   Row(children: <Widget>[
                     Expanded(child: 
-                      Text('商品描述',style: TextStyle(color: rgba(56, 56, 56, 1),fontSize: 13),),
-                    )
-                  ],),
-                  
-                  Row(children: <Widget>[
-                    Expanded(child: 
-                      Container(
-                        margin: EdgeInsets.only(top: 8),
-                        child: Text('浓缩咖啡与牛奶批次融合，加入祥龙巧克力风味。（建议到店引用，脑油融化钱口味更佳）\n主要原菜鸟：浓缩开发，牛娜，巧克力酱，交大奶油（喊小草风味糖浆），\n图片仅供参考，请以实物为准。建议送达后尽快引用。',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: rgba(128, 128, 128, 1)
-                          ),
-                        )
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15, right: 15),
+                        child: Text('大咖说', style: TextStyle(color: rgba(56, 56, 56, 1),fontSize: 13),),
                       )
-                    ,)
+                    ,),
+                  ],),
+
+                  Column(children: <Widget>[
+                    DacaiSay(
+                      avatar: 'lib/assets/images/menu/author1.jpg',
+                      name: 'Andrea Lattuada',
+                      label: 'WBC意大利冠军',
+                      desc: '咖啡好味道，生都八分。研磨、烘焙、虫蛀、萃取，我愿意就这样，用医生，去打磨一杯咖啡的乡村',
+                      time: '2018-09-22 10:22'
+                    ),
                   ],)
-              ],),
-            )
-          ],)
+                ],),  
+              ),
+            
+              // 商品描述
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Row(children: <Widget>[
+                      Expanded(child: 
+                        Text('商品描述',style: TextStyle(color: rgba(56, 56, 56, 1),fontSize: 13),),
+                      )
+                    ],),
+                    
+                    Row(children: <Widget>[
+                      Expanded(child: 
+                        Container(
+                          margin: EdgeInsets.only(top: 8),
+                          child: Text('浓缩咖啡与牛奶批次融合，加入祥龙巧克力风味。（建议到店引用，脑油融化钱口味更佳）\n主要原菜鸟：浓缩开发，牛娜，巧克力酱，交大奶油（喊小草风味糖浆），\n图片仅供参考，请以实物为准。建议送达后尽快引用。',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: rgba(128, 128, 128, 1)
+                            ),
+                          )
+                        )
+                      ,)
+                    ],)
+                ],),
+              )
+            ],)
+          ),
         ),
       ),
 

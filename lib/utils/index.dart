31566a385905bc2ec04285312147972bd8292c0b
meetqy <37372842@qq.com>
2019-09-24 14:47:23 +0800

    删除多余文件

--- lib/utils/index.dart
+++ lib/utils/index.dart
@@ -3,7 +3,4 @@ export './other.dart';
 export './commonUI.dart';
 export './customAppbar.dart';
 
-export 'package:flutter_luckin_coffee/utils/themes/ThemeColors.dart';
-export 'package:flutter_luckin_coffee/utils/themes/ThemeText.dart';
-
 

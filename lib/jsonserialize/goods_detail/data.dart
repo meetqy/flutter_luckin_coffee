a9b08f6b0da48b923ec293465c38d8869140d4d1
meetqy <37372842@qq.com>
2019-10-08 14:35:55 +0800

    增加商品详情序列化文件

---
+++ lib/jsonserialize/goods_detail/data.dart
@@ -0,0 +1,183 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'data.g.dart';
+
+@JsonSerializable()
+class GoodsDetail {
+  int code;
+  GoodsDetailData data;
+  String msg;
+
+  GoodsDetail({
+    this.code,
+    this.data,
+    this.msg,
+  });
+
+  //反序列化
+  factory GoodsDetail.fromJson(Map<String, dynamic> json) => _$GoodsDetailFromJson(json);
+  //序列化
+  Map<String, dynamic> toJson() => _$GoodsDetailToJson(this);
+}
+
+@JsonSerializable()
+class GoodsDetailData {
+  GoodsDetailExtJson extJson;
+  GoodsDetailCategory category;
+  List<GoodsDetailPic> pics;
+  String content;
+  GoodsDetailBasicInfo basicInfo;
+
+  GoodsDetailData({
+    this.extJson,
+    this.category,
+    this.pics,
+    this.content,
+    this.basicInfo,
+  });
+
+  //反序列化
+  factory GoodsDetailData.fromJson(Map<String, dynamic> json) => _$GoodsDetailDataFromJson(json);
+  //序列化
+  Map<String, dynamic> toJson() => _$GoodsDetailDataToJson(this);
+}
+
+@JsonSerializable()
+class GoodsDetailBasicInfo {
+  int categoryId;
+  String characteristic;
+  int commission;
+  int commissionType;
+  String dateAdd;
+  String dateUpdate;
+  int gotScore;
+  int gotScoreType;
+  int id;
+  bool kanjia;
+  int kanjiaPrice;
+  bool limitation;
+  int logisticsId;
+  bool miaosha;
+  int minPrice;
+  int minScore;
+  String name;
+  int numberFav;
+  int numberGoodReputation;
+  int numberOrders;
+  int numberSells;
+  double originalPrice;
+  int paixu;
+  String pic;
+  bool pingtuan;
+  int pingtuanPrice;
+  int recommendStatus;
+  String recommendStatusStr;
+  int shopId;
+  int status;
+  String statusStr;
+  int stores;
+  int userId;
+  int vetStatus;
+  int views;
+  int weight;
+
+  GoodsDetailBasicInfo({
+    this.categoryId,
+    this.characteristic,
+    this.commission,
+    this.commissionType,
+    this.dateAdd,
+    this.dateUpdate,
+    this.gotScore,
+    this.gotScoreType,
+    this.id,
+    this.kanjia,
+    this.kanjiaPrice,
+    this.limitation,
+    this.logisticsId,
+    this.miaosha,
+    this.minPrice,
+    this.minScore,
+    this.name,
+    this.numberFav,
+    this.numberGoodReputation,
+    this.numberOrders,
+    this.numberSells,
+    this.originalPrice,
+    this.paixu,
+    this.pic,
+    this.pingtuan,
+    this.pingtuanPrice,
+    this.recommendStatus,
+    this.recommendStatusStr,
+    this.shopId,
+    this.status,
+    this.statusStr,
+    this.stores,
+    this.userId,
+    this.vetStatus,
+    this.views,
+    this.weight,
+  });
+
+  //反序列化
+  factory GoodsDetailBasicInfo.fromJson(Map<String, dynamic> json) => _$GoodsDetailBasicInfoFromJson(json);
+  //序列化
+  Map<String, dynamic> toJson() => _$GoodsDetailBasicInfoToJson(this);
+}
+
+@JsonSerializable()
+class GoodsDetailCategory {
+  String dateAdd;
+  int id;
+  bool isUse;
+  String name;
+  int paixu;
+  int pid;
+  int userId;
+
+  GoodsDetailCategory({
+    this.dateAdd,
+    this.id,
+    this.isUse,
+    this.name,
+    this.paixu,
+    this.pid,
+    this.userId,
+  });
+
+  //反序列化
+  factory GoodsDetailCategory.fromJson(Map<String, dynamic> json) => _$GoodsDetailCategoryFromJson(json);
+  //序列化
+  Map<String, dynamic> toJson() => _$GoodsDetailCategoryToJson(this);
+}
+
+@JsonSerializable()
+class GoodsDetailExtJson {
+  GoodsDetailExtJson();
+
+   //反序列化
+  factory GoodsDetailExtJson.fromJson(Map<String, dynamic> json) => _$GoodsDetailExtJsonFromJson(json);
+  //序列化
+  Map<String, dynamic> toJson() => _$GoodsDetailExtJsonToJson(this);
+}
+
+@JsonSerializable()
+class GoodsDetailPic {
+  int goodsId;
+  int id;
+  String pic;
+  int userId;
+
+  GoodsDetailPic({
+    this.goodsId,
+    this.id,
+    this.pic,
+    this.userId,
+  });
+
+  //反序列化
+  factory GoodsDetailPic.fromJson(Map<String, dynamic> json) => _$GoodsDetailPicFromJson(json);
+  //序列化
+  Map<String, dynamic> toJson() => _$GoodsDetailPicToJson(this);
+}

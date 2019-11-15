6f8cd953c7bcd54c5dcce71b8f620bbb2fe5f2a9
meetqy <37372842@qq.com>
2019-11-15 10:59:18 +0800

    解决安卓不能打包问题

---
+++ android/app/src/main/kotlin/com/example/flutter_luckin_coffee_copy/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.flutter_luckin_coffee_copy
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
